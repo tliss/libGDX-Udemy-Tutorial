f699388ef6c72464c319fda38efb6b5a46bdfdc9
Taylor Liss <tliss@camsys.com>
2017-12-04 14:10:09 -0500

    Finish lesson 17

--- My LibGDX Tester/core/src/com/samort7/mylibgdxtester/GameMain.java
+++ My LibGDX Tester/core/src/com/samort7/mylibgdxtester/GameMain.java
@@ -4,27 +4,35 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import helpers.GameInfo;
 
 public class GameMain extends Game {
 	SpriteBatch batch;
 	Texture img;
-	Texture img2;
+	Sprite player;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("Game BG.png");
-		img2 = new Texture("badlogic.jpg");
+		player = new Sprite(new Texture("Player 1.png"));
+		player.setPosition((GameInfo.WIDTH / 2) - (player.getWidth()/2),
+				(GameInfo.HEIGHT/2) - (player.getHeight()/2));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		player.setPosition(player.getX(), player.getY());
+
 		batch.begin();
 		batch.draw(img, 0, 0);
-		batch.draw(img2, 480/2 - img2.getWidth() / 2, 800/2 - img2.getWidth() / 2);
+		batch.draw(player, player.getX(), player.getY());
 		batch.end();
 	}
 
---
+++ My LibGDX Tester/core/src/helpers/GameInfo.java
@@ -0,0 +1,11 @@
+package helpers;
+
+/**
+ * Created by taylorliss on 12/4/17.
+ */
+
+public class GameInfo {
+
+    public static final int WIDTH = 480;
+    public static final int HEIGHT = 800;
+}
