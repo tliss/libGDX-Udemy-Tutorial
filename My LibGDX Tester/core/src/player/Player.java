690ad3f9f69167dd6c98d084465c999f1b0a53db
Taylor Liss <tayloraliss@gmail.com>
2017-12-09 13:35:27 -0500

    Finish lesson 21

--- My LibGDX Tester/core/src/player/Player.java
+++ My LibGDX Tester/core/src/player/Player.java
@@ -1,7 +1,14 @@
 package player;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.PolygonRegion;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
 /**
  * Created by samor on 12/5/2017.
@@ -9,9 +16,34 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Player extends Sprite {
 
-    public Player(String name, float x,  float y) {
+    private World world;
+    private Body body;
+
+    public Player(World world, String name, float x,  float y) {
         super(new Texture(name));
+        this.world = world;
         setPosition(x - getWidth() / 2, y - getHeight() / 2);
+        createBody();
     }
 
+    void createBody() {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(getX(), getY());
+        body = world.createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(getWidth() / 2, getHeight() / 2);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1f;
+        Fixture fixture = body.createFixture(fixtureDef);
+
+        shape.dispose();
+    }
+
+    public void updatePlayer(){
+        this.setPosition(body.getPosition().x, body.getPosition().y);
+    }
 }
--- My LibGDX Tester/core/src/scenes/MainMenu.java
+++ My LibGDX Tester/core/src/scenes/MainMenu.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 import com.samort7.mylibgdxtester.GameMain;
 
 import helpers.GameInfo;
@@ -18,17 +20,16 @@ import player.Player;
 public class MainMenu implements Screen {
 
     private GameMain game;
-
     private Texture bg;
-
     private Player player;
+    private World world;
 
     //Constructor for thsi class
     public MainMenu(GameMain game) {
         this.game = game;
+        world = new World(new Vector2(0, -9.8f), true);
         bg = new Texture("Game BG.png");
-        player = new Player("Player 1.png", GameInfo.WIDTH / 2, GameInfo.HEIGHT /2);
-
+        player = new Player(world, "Player 1.png", GameInfo.WIDTH / 2, GameInfo.HEIGHT /2);
     }
 
     @Override
@@ -39,6 +40,8 @@ public class MainMenu implements Screen {
     @Override
     public void render(float delta) {
 
+        player.updatePlayer();
+
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -47,6 +50,7 @@ public class MainMenu implements Screen {
         game.getBatch().draw(player, player.getX(), player.getY());
         game.getBatch().end();
 
+        world.step(Gdx.graphics.getDeltaTime(), 6, 2);
     }
 
     @Override
@@ -71,6 +75,7 @@ public class MainMenu implements Screen {
 
     @Override
     public void dispose() {
-
+        bg.dispose();
+        player.getTexture().dispose();
     }
 }
