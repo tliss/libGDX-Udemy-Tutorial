166dfcacc1078d8963a33ff51364d370e14f045f
Taylor Liss <tayloraliss@gmail.com>
2017-12-16 13:39:30 -0500

    Finish lesson 29

--- Jack The Giant/core/src/scenes/Gameplay.java
+++ Jack The Giant/core/src/scenes/Gameplay.java
@@ -1,19 +1,34 @@
 package scenes;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.tayloraliss.jackthegiant.GameMain;
+
+import helpers.GameInfo;
 
 public class Gameplay implements Screen {
 
     private GameMain game;
+
+    private OrthographicCamera mainCamera;
+    private Viewport gameViewport;
+
     private Sprite[] bgs;
 
     public Gameplay(GameMain game){
         this.game = game;
+
+        mainCamera = new OrthographicCamera(GameInfo.WIDTH, GameInfo.HEIGHT);
+        mainCamera.position.set(GameInfo.WIDTH/2f, GameInfo.HEIGHT/2f, 0);
+
+        gameViewport = new StretchViewport(GameInfo.WIDTH, GameInfo.HEIGHT, mainCamera);
 
         createBackgrounds();
     }
@@ -24,6 +39,14 @@ public class Gameplay implements Screen {
             bgs[i] = new Sprite(new Texture("Backgrounds/Game BG.png"));
             bgs[i].setPosition(0, -(i * bgs[i].getHeight()));
         }
+    }
+
+    void update(float dt){
+        moveCamera();
+    }
+
+    void moveCamera(){
+        mainCamera.position.y -= 1;
     }
 
     void drawBackgrounds() {
@@ -39,6 +62,9 @@ public class Gameplay implements Screen {
 
     @Override
     public void render(float delta) {
+
+        update(delta);
+
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -47,6 +73,9 @@ public class Gameplay implements Screen {
         drawBackgrounds();
 
         game.getBatch().end();
+
+        game.getBatch().setProjectionMatrix(mainCamera.combined);
+        mainCamera.update();
     }
 
     @Override
