ff356a3be71fe429fdf7cb0486b4698d725bbbe7
Taylor Liss <tayloraliss@gmail.com>
2017-12-16 12:41:59 -0500

    Finish lesson 28

--- Jack The Giant/core/src/scenes/Gameplay.java
+++ Jack The Giant/core/src/scenes/Gameplay.java
@@ -3,6 +3,7 @@ package scenes;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.tayloraliss.jackthegiant.GameMain;
 
@@ -13,6 +14,22 @@ public class Gameplay implements Screen {
 
     public Gameplay(GameMain game){
         this.game = game;
+
+        createBackgrounds();
+    }
+
+    void createBackgrounds() {
+        bgs = new Sprite[3];
+        for (int i = 0; i < bgs.length; i++){
+            bgs[i] = new Sprite(new Texture("Backgrounds/Game BG.png"));
+            bgs[i].setPosition(0, -(i * bgs[i].getHeight()));
+        }
+    }
+
+    void drawBackgrounds() {
+        for (int i=0; i < bgs.length; i++) {
+            game.getBatch().draw(bgs[i], bgs[i].getX(), bgs[i].getY());
+        }
     }
 
     @Override
@@ -26,6 +43,8 @@ public class Gameplay implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.getBatch().begin();
+
+        drawBackgrounds();
 
         game.getBatch().end();
     }
