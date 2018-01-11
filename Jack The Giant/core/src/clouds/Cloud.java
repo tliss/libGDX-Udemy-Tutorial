7de207a91c503498c4fc5393622ffc6340ac77a6
Taylor Liss <tayloraliss@gmail.com>
2018-01-10 21:56:50 -0500

    Finish lesson 31

--- Jack The Giant/core/src/clouds/Cloud.java
+++ Jack The Giant/core/src/clouds/Cloud.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 import helpers.GameInfo;
@@ -26,6 +29,26 @@ public class Cloud extends Sprite{
 
         bodyDef.position.set((getX() + getWidth() / 2) / GameInfo.PPM,
                 (getY() + getHeight() / 2) / GameInfo.PPM);
+
+        body = world.createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox((getWidth() / 2) / GameInfo.PPM,
+                (getHeight() /2) / GameInfo.PPM);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        Fixture fixture = body.createFixture((fixtureDef));
+
+        shape.dispose();
     }
 
+    public void setSpritePosition(float x, float y) {
+        setPosition(x, y);
+        createBody();
+    }
+
+    public String getCloudName(){
+        return this.cloudName;
+    }
 }
